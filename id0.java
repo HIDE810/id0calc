1904f505e1b5abe3d9c87691bf99435fff31ee08
HIDE <48782538+HIDE810@users.noreply.github.com>
2020-10-19 21:53:32 +0900

    Update id0.java

--- id0.java
+++ id0.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.*;
 import java.util.*;
 import java.security.*;
 
@@ -11,29 +12,17 @@ class ID0 {
             raf.read(bytes);
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] hash = digest.digest(bytes);
-            
-            byte[] id0 = new byte[0x10];
             byte[] tmp = new byte[4];
             
             for(int i = 0; i < 4; i++) {
                 System.arraycopy(hash, i * 4, tmp, 0, 4);
-                
-                for(int j = 3; j >= 0; j--)
-                    System.arraycopy(tmp, j, id0, i * 4 + 3 - j, 1);
-            }
-            
-            for(byte b : id0) {
-                System.out.print(String.format("%02X", b));
+                ByteBuffer buf = ByteBuffer.wrap(tmp);
+                buf.order(ByteOrder.LITTLE_ENDIAN);
+                System.out.printf("%08X", buf.getInt(0));
             }
         }
-        catch(FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch(IOException e) {
-            e.printStackTrace();
-        }
-        catch(NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
+        catch(FileNotFoundException e) {e.printStackTrace();}
+        catch(IOException e) {e.printStackTrace();}
+        catch(NoSuchAlgorithmException e) {e.printStackTrace();}
     }
 }
